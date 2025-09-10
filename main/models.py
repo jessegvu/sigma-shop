451fe9a37a89a04e29c25e2e7cda07aa6eb0e541
jessegvu <gojessgerard@gmail.com>
2025-09-10 08:54:34 +0700

    Complete tugas mandiri 2: Django MVT implementation

---
+++ main/models.py
@@ -0,0 +1,33 @@
+import uuid
+from django.db import models
+
+# Create your models here.
+class Product(models.Model):
+    CATEGORY_CHOICES = [
+        ('transfer', 'Transfer'),
+        ('update', 'Update'),
+        ('exclusive', 'Exclusive'),
+        ('match', 'Match'),
+        ('rumor', 'Rumor'),
+        ('analysis', 'Analysis'),
+    ]
+    
+    name = models.CharField(max_length=255)
+    price = models.IntegerField(max_length=255)
+    description = models.TextField(max_length=255)
+    thumbnail = models.URLField
+    category = models.CharField(max_length=255)
+    thumbnail = models.URLField(blank=True, null=True)
+    is_featured = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return self.title
+    
+    @property
+    def is_news_hot(self):
+        return self.news_views > 20
+        
+    def increment_views(self):
+        self.news_views += 1
+        self.save()
+
